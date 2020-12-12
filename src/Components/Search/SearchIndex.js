d80a9a855b53a3d6ca13130e31d062eae9a7dbc7
Jillian Howell <40151421+jilherma@users.noreply.github.com>
2020-12-12 15:58:47 -0500

    first commit

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import {Input} from 'reactstrap';
- 
-const SearchIndex extends Component() {
-   this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
- }
-
- function searchFunction() {
- }
-
- render() {
-     <div>
-       <Input placeholder='Search Here' />
-       <h3>Results:</h3>
-     </div>
- }
-
- 
-export SearchIndex;
+import React, { Component } from 'react';
+import { Input } from 'reactstrap';
+class SearchIndex extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'],
+    }
+  }
+// Make a state for a search term
+// Set search term equal to value of input
+// Use array method to check if the value equals one of the strings in the array
+// Render
+  searchFunction(e) {
+    this.setState({
+      searchTerm: (e)
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Input type='text' placeholder='Search Here' onChange={(e) => this.searchFunction(e.target.value)}/>
+        <h3>Results:</h3>
+        {this.state.things
+        .filter((thing) => {
+          if (thing.includes(this.state.searchTerm)){
+            return true
+          } else {
+            return null
+          }
+        })
+        .map((thing, index) => {
+          return(
+            <div key={index}>
+              <h3>{thing}</h3>
+              </div>
+          )
+        })
+        }
+      </div>
+    );
+  }
+}
+export default SearchIndex;
